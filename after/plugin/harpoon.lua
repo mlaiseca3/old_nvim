59d998f22e45641dcccb872f628a6b3b52294e7b
Mario L <mariolaisecaruiz@gmai.com>
2024-01-03 20:36:19 -0600

    added remove item from harpoon list

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -5,6 +5,7 @@ harpoon:setup()
 -- REQUIRED
 
 vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
+vim.keymap.set("n", "<leader>r", function() harpoon:list():remove() end)
 vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
 vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
